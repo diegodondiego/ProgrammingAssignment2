73f173e5eee36cf65997427bf7523ffcc85710b7
diegodondiego <dondiego7@gmail.com>
2015-07-26 05:15:41 -0300

    Finalizando os métodos pedidos

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,48 @@
 
-makeCacheMatrix <- function(x = matrix()) {
+# cache the matrix
+makeCacheMatrix <- function(theMatrix = matrix()) {
         invertedMatrix <- NULL
         
+        # basic functions
+        get <- function() { return(theMatrix) }
+        set <- function (y) {
+                matrix <<- y
+                invertedMatrix <<- NULL
+        }
+        
+        # inverting all
+        getInvertedMatrix <- function() { return( invertedMatrix ) }
+        setInvertedMatrix <- function(aInvertedMatrix) { invertedMatrix <<- aInvertedMatrix }
+        
+        # create the list with the returns
+        list(get = get, set = set, setinverse = setInvertedMatrix, getinverse = getInvertedMatrix)
 }
 
+# resolve the cache for the inverted
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(aCachedMatrix, ...) {
+        
+        cachedInverted <- aCachedMatrix$getinverse()
+        
+        if (!is.null(x = cachedInverted)) {
+                print("cache hit! (:")
+                return(cachedInverted)
+        }
+        
+        print("cache miss! ):")
+        
+        tryCatch({
+                # try to set the cached version
+                aCachedMatrix$setinverse( solve( aCachedMatrix$get(), ... ) )
+                },
+                error = function(e) {
+                        message("Error! Problably it's impossible to invert the matrix.")
+                        message(e)
+                        
+                        return(NA)
+                }
+        )
+        
         ## Return a matrix that is the inverse of 'x'
+        return( aCachedMatrix$getinverse() )
 }
